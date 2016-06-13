a09483b62e95a6e8769742f094b45f3b59b0bb84
jeffryporter <jporter7@gmail.com>
2016-06-13 10:48:33 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.theironyard.javawithclojure.jhporter;
+
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
+
+public class Main
+{
+
+    public static void main(String[] args) throws SQLException
+    {
+        Server.createWebServer().start();
+
+
+
+    }
+}
